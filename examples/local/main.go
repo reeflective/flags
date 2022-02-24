e4391f529c5c06d5a9b48aae7fe6bbfd6fcd37c4
maxlandon <maximelandon@gmail.com>
2022-02-23 19:31:11 -0500

    Add a first draft of the local example package

---
+++ examples/local/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/maxlandon/reflags"
+)
+
+func main() {
+
+	// The main function is used to demonstrate how to:
+	// - Create and set up a command Parser (app)
+	// - Bind commands to it.
+	// - Further customize/adjust either commands or parser
+	// - Run the application
+
+	// 1 - Creating the parser
+	client := reflags.NewClient("local", reflags.Default)
+
+	// 2 - Binding commands
+	client.AddCommand("list",
+		"A local command demonstrating a few reflags features",
+		"A longer help string used in detail help/usage output",
+		"", // We don't use a group for this first example. See later.
+		&Command{},
+	)
+
+	fmt.Println("bound commands !")
+
+	// 4 - Running the application
+	if err := client.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
